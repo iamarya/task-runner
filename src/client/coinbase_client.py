f83a2c8c260b94bf93cb0ec8d2763fe8cf2a73a3
Arya Mishra <aryamishra.in@gmail.com>
2025-09-01 12:28:19 +0100

    buy btc task

--- src/client/coinbase_client.py
+++ src/client/coinbase_client.py
@@ -67,7 +67,7 @@ import asyncio
 async def main():
     cb_client = CoinbaseClient()
     loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(None, cb_client.get_usdc_balance)
+    return await loop.run_in_executor(None, cb_client.buy_btc_with_usdc)
 
 if __name__ == "__main__":
     asyncio.run(main())
